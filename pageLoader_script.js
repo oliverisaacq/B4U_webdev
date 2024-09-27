ab719627b5e556c20dd1230193f0281cb5ec8b6e
O L I V E R <83379794+oliverisaacq@users.noreply.github.com>
2024-09-27 16:00:43 -0500

    algunas cambios lele

--- pageLoader_script.js
+++ pageLoader_script.js
@@ -3,6 +3,8 @@ window.history.scrollRestoration = 'manual'; // Evitar que el navegador mantenga
 window.scrollTo(0, 0); // Asegurarse de que la página empiece en la parte superior
 
 window.addEventListener('load', () => {
+    // Forzar el scroll a la parte superior y omitir cualquier ancla en la URL
+    window.scrollTo(0, 0);  // Volver a la parte superior de la página
     const loaderArea = document.querySelector('.loader-area');
     const logoBlack = document.querySelector('.loader-logo-black');
     const container = document.querySelector('.loader-container');
@@ -36,7 +38,7 @@ window.addEventListener('load', () => {
 
             // Reactivar el scroll después de cargar la página
             body.classList.remove('no-scroll');
-            body.classList.add('page-loaded'); // Activar la animación de la página principal
+            body.classList.add('page-loaded'); // Activar la animación de la página principal            
         }, 1000);
     }, 4000);
 });
